c16f3580d4d3b2b090d023a9ebe2c8c72a27f2ed
guidugaich <guidugaich.dev@gmail.com>
2021-05-14 16:21:12 -0300

    feat(req2): :sparkles: Adicionando Switch no BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-      <Route to="/" component={ProductList} />
+      <Switch>
+        <Route to="/" component={ProductList} />
+      </Switch>
     </BrowserRouter>
   );
 }

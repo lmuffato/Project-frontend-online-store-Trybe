af19614e3da319cb73aced859560c2f3380ed13e
luanlsr <luanfswd@gmail.com>
2021-05-14 16:50:03 -0300

    Finalizado o item 1 do requisito 01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 import Routes from './components/Routes';
 
 function App() {
@@ -9,4 +10,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,11 +4,16 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
+        {/* <label htmlFor="home-initial-message"> */}
         <input
+          className="home-initial-message"
+          id="home-initial-message"
           data-testid="home-initial-message"
           type="search"
-          placeholder="'Digite algum termo de pesquisa ou escolha uma categoria.'"
+          placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
         />
+        {/* </label> */}
+        <h3>Digite algum termo de pesquisa ou escolha uma categoria.</h3>
       </div>
     );
   }

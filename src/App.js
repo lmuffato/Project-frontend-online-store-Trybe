2320a55b15a1e57a9a2770397d8e00b0c44a9d7e
Jodiel Briesemeister <jodielll29@gmail.com>
2021-05-14 15:50:06 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductList from './components/product_list';
 
 function App() {
   return (
     <div>
-      A
+      <Router>
+        <Route exact path="/" component={ ProductList } />
+      </Router>
     </div>
   );
 }

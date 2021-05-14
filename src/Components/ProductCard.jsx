c213dcbee42c4de0fe3814b644a0c479f618e3a5
mallmann02 <leomallmann020302@gmail.com>
2021-05-14 19:37:25 -0300

    feat: Add Props Definition

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ProductCard extends Component {
   render() {
     const { title, price, imagePath } = this.props;
-    return(
+    return (
       <div>
         <h1 data-testid="product">{title}</h1>
-        <img data-testid="product" src={imagePath}/>
-        <span data-testid="product" >{price}</span>
+        <img data-testid="product" src={ imagePath } alt="Imagem do produto" />
+        <span data-testid="product">{price}</span>
       </div>
     );
   }
 }
+
+ProductCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
 
 export default ProductCard;

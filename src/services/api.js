cd701c7a00e99c564bcad08797fd2204a15123db
guilhermemd <guilherme.md@gmail.com>
2021-05-14 17:40:22 -0300

    getProductsFromCategoryAndQuery implement

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,12 @@ export async function getCategories() {
   const categoriesResponse = await fetchCategories.json();
   return categoriesResponse;
 }
-console.log(getCategories);
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const id = categoryId;
+  const url = `https:api.mercadolibre.com/sites/MLB/search?category=${id}&q=${query}`;
+  const searchBy = await fetch(url);
+  const json = await searchBy.json();
+  console.log(json);
+  return json;
 }

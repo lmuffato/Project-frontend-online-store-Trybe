292d3c596b299c16770add2f7aa1e23151be0c6c
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-14 19:07:53 -0300

    feat: requirement03

--- src/components/Routers.js
+++ src/components/Routers.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
+import ShoppingCart from '../pages/ShoppingCart';
 
 export default class Routers extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/shopping-cart" component={ ShoppingCart } />
         <Route exact path="/" component={ Home } />
       </Switch>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <input type="seach" />
+        <input type="text" />
         <p
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link
+          to="/shopping-cart"
+          data-testid="shopping-cart-button"
+        >
+          Cart
+        </Link>
       </div>
     );
   }

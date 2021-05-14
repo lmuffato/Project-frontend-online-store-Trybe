8ba3e2dc91c72c8c4d50657e0b8ffc1644077794
Renata Andrade <reapminas@gmail.com>
2021-05-14 16:19:54 -0300

    feat: requisiçao da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const data = await fetch ('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await data.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const data = await fetch (`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const product = await data.json();
+  return product;
 }

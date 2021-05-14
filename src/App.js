f0b1698f1e6a3bebc87ae5b03f6ef0ffcf98bdfe
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-05-14 17:02:13 -0300

    feat: create component shopping cart

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/shoppingcart" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     );

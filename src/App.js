7774d4b481b6e3254a4e8ca126e223d633f243e7
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-14 15:26:42 -0400

    fix: eslint problems

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { getCategories } from './services/api'
 
-function App() {
-  
+function App() {  
   return (
     <div className="App">
-      { console.log(getCategories()) }
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>Edit src/App.js and save to reload.</p>
--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ export async function getCategories() {
   return data;
 }
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${ categoryId }&q=${ query }`);
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const data = await response.json();
   return data;
 }

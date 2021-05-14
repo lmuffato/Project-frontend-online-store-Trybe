f2832b3d38ca1493082dba59d6a8f30e3193a65f
mallmann02 <leomallmann020302@gmail.com>
2021-05-14 15:59:07 -0300

    fix: Corrige implementaçao do requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 export async function getCategories() {
-  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((data) => data.json());
   return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const products = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const products = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((data) => data.json());
   return products;
 }

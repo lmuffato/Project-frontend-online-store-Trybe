8cdcbb0a1573f4e4cf9ac31e075988160e9783a7
root <root@DESKTOP-74CRLKE.localdomain>
2021-05-14 16:12:38 -0300

    req 2 - pagina de listagem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="input">
+          <input type="text" id="input" />
+        </label>
+        <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+      </div>
+    );
+  }
 }
 
 export default App;

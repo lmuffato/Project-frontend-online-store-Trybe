43df3e741b121b6a70b480b19dbf66cf07a8f00a
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-05-14 19:18:12 -0300

    feat: Requisito 3, concluido / Implementado botao que leva ao carrinho, inicialmente vazio, assim como uma pagina (/cart) para o carrinho

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import * as api from './services/api';
+// import * as api from './services/api';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
-  api.getCategories().then((categories) => { console.log(categories); });
-  api.getProductsFromCategoryAndQuery('MLB5672', 'roda')
-    .then((categories) => { console.log(categories); });
+  // api.getCategories().then((categories) => { console.log(categories); });
+  // api.getProductsFromCategoryAndQuery('MLB5672', 'roda')
+  //   .then((categories) => { console.log(categories); });
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route path="/" exact component={ Home } />
+          <Route path="/cart" exact component={ Cart } />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">
+        Seu carrinho está vazio
+      </p>
+    );
+  }
+}
+
+export default Cart;

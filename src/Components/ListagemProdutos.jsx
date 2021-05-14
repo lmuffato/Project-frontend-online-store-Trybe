0c0ab7548881122868bdbb6c751aad759a54b97f
Lucas André <lucas.cancado89@gmail.com>
2021-05-14 18:17:09 -0300

    feat: categories (req4)

--- src/Components/ListagemProdutos.jsx
+++ src/Components/ListagemProdutos.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Categories from './Categories';
 
 class ListagemProdutos extends Component {
   render() {
     return (
       <div>
+        <Categories />
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.

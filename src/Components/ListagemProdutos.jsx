7653d4ab7fcef88e188fbf8efd0f4496073f32e4
Rafael Ribeiro <rafittu@outlook.com>
2021-05-14 17:40:28 -0300

    adiciona botao carrinho - req 3

--- src/Components/ListagemProdutos.jsx
+++ src/Components/ListagemProdutos.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ListagemProdutos extends Component {
   render() {
     return (
       <div>
         <input type="text" />
+        <aside>
+          <Link to="/cart">
+            <button type="button" data-testid="shopping-cart-button">Cart</button>
+          </Link>
+        </aside>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>

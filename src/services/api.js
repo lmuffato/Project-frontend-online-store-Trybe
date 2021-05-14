789ea271b53cf5b12f31e0ecac1f7dc05306af56
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-14 16:51:24 -0300

    resolve requisito 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await data.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(category, query) {
+  const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${category}&q=${query}`);
+  const querycategory = await data.json();
+  return querycategory;
 }

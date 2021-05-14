6730c99a44d9939aa5911fa3ccfe83eca4a6c2e3
Adelino Junior <78621950+AdelinoJnr@users.noreply.github.com>
2021-05-14 17:02:24 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,10 +5,6 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // const requestQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-  // const requestId = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
-  // const dataId = await requestId.json();
-  // const dataQuery = await requestQuery.json();
   const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const data = await request.json();
 

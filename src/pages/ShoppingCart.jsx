0234e1a1d776821f2133ec8063926b7d429baf67
Iago de Paula <iago.pferreiravr@gmail.com>
2021-05-14 18:35:45 -0300

    Requisito 3 - pairPrograming Heloisa/Iago

---
+++ src/pages/ShoppingCart.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    );
+  }
+}

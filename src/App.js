e9b6b277ddbb6f485739b4f401a6661682ea10b5
jlfagundes <jlfagundes@gmail.com>
2021-05-14 17:24:28 -0300

    Feat: (req3) implementation new page ShoppingCart

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={ Home } exact />
+        <Route path="/shoppingcart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
@@ -8,7 +9,9 @@ class Home extends React.Component {
         <strong data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </strong>
-        <button type="button" data-testid="shopping-cart-button">Carrinho</button>
+        <Link to="/shoppingcart">
+          <button type="button" data-testid="shopping-cart-button">Carrinho</button>
+        </Link>
       </>
     );
   }

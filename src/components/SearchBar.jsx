0508ae66ee9e0045d44e6961d39620d9418e0361
luanlsr <luanfswd@gmail.com>
2021-05-14 17:03:41 -0300

    requisito 02 finalizado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,17 +3,16 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <div>
+      <div data-testid="home-initial-message">
         {/* <label htmlFor="home-initial-message"> */}
         <input
           className="home-initial-message"
           id="home-initial-message"
-          data-testid="home-initial-message"
           type="search"
           placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
         />
         {/* </label> */}
-        <h3>Digite algum termo de pesquisa ou escolha uma categoria.</h3>
+        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
       </div>
     );
   }

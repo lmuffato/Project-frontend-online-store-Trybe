1c9b8391b89bc661c5eaafc293bddbf24113cf06
Stanrley Barbosa <3011stan@gmail.com>
2021-05-14 17:09:49 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ SearchBar } />
+      <Route path="/shopping-cart" component={ ShoppingCart } />
     </BrowserRouter>
   );
 }

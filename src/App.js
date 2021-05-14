84e8dbd583bead94f7209b4c12139926c3673b14
Viegas <mvrdgs@gmail.com>
2021-05-14 15:47:19 -0300

    fix: lint error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import * as api from './services/api'
+import * as api from './services/api';
 
 function App() {
   console.log(api.getCategories());
   return (
-    <div>
-    </div>
+    <div />
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ export async function getCategories() {
   return (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, /* query */) {
+export async function getProductsFromCategoryAndQuery(categoryId /* query */) {
   return (await fetch(`https://api.mercadolibre.com/sites/MLB/categories/${categoryId}`)).json();
 }

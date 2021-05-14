bb908ab8ea7a8d6304fdede0e8a4b7fb62db5ff1
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-14 16:53:20 -0300

    feat: Requirement01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fetchApiCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const resolve = await fetchApiCategories.json();
+  return resolve;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const fetchApiQuery = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY');
+  const resolve = await fetchApiQuery.json();
+  return resolve;
 }

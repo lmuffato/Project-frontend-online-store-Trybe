7825c05e83ce07f06d5991a82b00ac2334dd3d54
LucasMLara <lucasml@live.com>
2021-05-14 16:39:53 -0300

    GetCategories Implemented

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categoriesResponse = await fetchCategories.json();
+  return categoriesResponse;
 }
+console.log(getCategories);
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe

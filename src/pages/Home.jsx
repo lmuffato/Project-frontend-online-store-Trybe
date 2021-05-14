0fada2a03ad297f50898b3e6bab4d0ddbbc88d35
Renzo Sevilha <sevilharenzo@gmail.com>
2021-05-14 19:07:47 -0300

    feat: req4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
       <section>
+        <input type="text" />
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <ul>
+          {categories.map(({ id, name }) => (
+            <li
+              key={ id }
+              data-testid="category"
+            >
+              {name}
+            </li>))}
+        </ul>
       </section>
     );
   }

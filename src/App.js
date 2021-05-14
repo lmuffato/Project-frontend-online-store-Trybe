3db6793df266b5f6a76dd708165528d621be5614
Villefor <franklincrius@gmail.com>
2021-05-14 19:11:00 -0300

    sugestao do requisito3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SearchBar from './pages/SearchBar';
+import CartContent from './pages/CartContent';
 
 function App() {
   return (
     <Router>
       <Route path="/" component={ SearchBar } />
+      <Route path="/CartContent" component={ CartContent } />
     </Router>
   );
 }

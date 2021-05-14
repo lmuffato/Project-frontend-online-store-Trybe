4841fcf12e3c74140b43f592363f4c1e08b22f21
Mateus Alencar <mtauws@gmail.com>
2021-05-14 16:21:37 -0300

    feat: requirement 2

---
+++ src/components/InputSearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class InputSearch extends Component {
+  render() {
+    return (
+      <div>
+        <input type="search" />
+        <h2 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+      </div>
+    );
+  }
+}
+
+export default InputSearch;

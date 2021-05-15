696a5ca2c8c402cfa4aa9e38c5b80f0bcf393ce9
defauth98 <neto.daniribeiro@gmail.com>
2021-05-15 08:17:54 -0300

    feat: Requisito 04 - Add CategoriesBar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
+import CategoriesBar from '../components/CategoriesBar';
 import SearchBar from '../components/SearchBar';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    const categories = await getCategories();
+
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <>
-        <SearchBar />
-        <Button>
-          <Link data-testid="shopping-cart-button" to="/Cart">Cart</Link>
-        </Button>
-      </>
+      <main>
+        <CategoriesBar categories={ categories } />
+        <section>
+          <SearchBar />
+          <Button>
+            <Link data-testid="shopping-cart-button" to="/Cart">Cart</Link>
+          </Button>
+        </section>
+
+      </main>
     );
   }
 }

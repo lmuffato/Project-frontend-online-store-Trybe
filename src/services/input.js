b3bdfbe7932b81ca7987c5158ca514ebe904b0d9
root <root@DESKTOP-74CRLKE.localdomain>
2021-05-14 23:50:17 -0300

    requisito 5

---
+++ src/services/input.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+import * as api from './api';
+
+class Input extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      products: [],
+      loading: false,
+    };
+  }
+
+    inputValue = ({ target }) => {
+      this.setState({
+        input: target.value,
+      });
+    }
+
+    fecthAPI = async () => {
+      const { input } = this.state;
+      this.setState(
+        { loading: true },
+        async () => {
+          const { results } = await api.getProductsFromCategoryAndQuery(input);
+          this.setState({
+            products: results,
+            loading: false,
+          });
+        },
+      );
+    }
+
+    render() {
+      const { products, loading } = this.state;
+      if (loading) return (<p>Carregando...</p>);
+      return (
+        <div>
+          <label htmlFor="input">
+            <input
+              type="text"
+              id="input"
+              data-testid="query-input"
+              onChange={ (e) => this.inputValue(e) }
+            />
+          </label>
+          <button data-testid="query-button" type="button" onClick={ this.fecthAPI }>
+            Pesquisar
+          </button>
+          <h3
+            data-testid="home-initial-message"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h3>
+
+          {
+            products === []
+              ? (<p>Nenhum produto foi encontrado</p>)
+              : products.map(({ title, price, thumbnail, id }) => (
+                <ProductCard
+                  key={ id }
+                  title={ title }
+                  price={ price }
+                  imagePath={ thumbnail }
+                />
+              ))
+          }
+
+        </div>
+      );
+    }
+}
+
+export default Input;

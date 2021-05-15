8fe22effcac6b3b2353c4c7c188ddef43c608d17
Villefor <franklincrius@gmail.com>
2021-05-15 06:24:06 -0300

    Requisito 4 feito, ajeitei um pouco do css para ficar parecido com o sugerido pelo projeto, botei img de carro vazio no CartContent

--- src/pages/CartContent.jsx
+++ src/pages/CartContent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import emptyCart from './images/emptyCart.png';
 
 class CartContent extends Component {
   render() {
@@ -6,6 +7,8 @@ class CartContent extends Component {
       <section style={ { textAlign: 'center' } }>
         <p data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
+          <br />
+          <img style={ { width: '100px' } } src={ emptyCart } alt="Carrinho vázio" />
         </p>
       </section>
     );

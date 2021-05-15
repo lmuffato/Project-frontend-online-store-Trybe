48079a76c0a13e7c97a061b6bf0b4c9466b68145
mallmann02 <leomallmann020302@gmail.com>
2021-05-15 00:10:57 -0300

    fix: Imports Ordenation

--- src/Components/ListagemProdutos.jsx
+++ src/Components/ListagemProdutos.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import * as api from '../services/api';
 
 import ProductCard from './ProductCard';
-
-import { Link } from 'react-router-dom';
-
 import Categories from './Categories';
 
 class ListagemProdutos extends Component {

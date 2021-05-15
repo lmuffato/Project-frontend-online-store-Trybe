19a66acfbdba6545745a0919146fa2a72df12d6c
lucas-paz <lucas.hpaz@outlook.com>
2021-05-15 11:02:25 -0300

    Cria o componente Item da lista de resultados e torna o searchBar em componente controlado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      itemToSearch: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { itemToSearch } = this.state;
     return (
       <header>
         <label htmlFor="input-search" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-          <input type="text" id="input-search" />
+          <input
+            name="itemToSearch"
+            type="text"
+            id="input-search"
+            onChange={ this.handleChange }
+            value={ itemToSearch }
+          />
         </label>
       </header>
     );
   }
 }
+
+// SearchBar.propTypes = {
+// }.isRequired;
 
 export default SearchBar;

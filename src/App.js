0755caa77c1cead63afc8fe6172f4619f3b0bd53
Lucas Muffato <lmiremuffato@gmail.com>
2021-05-17 15:40:39 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import ProductList from './Pages/ProductList';
+import ShoppingCart from './Pages/ShoppingCart';
+
 
 function App() {
   return (
-    <ProductList />
+    <section>
+      <BrowserRouter>
+        <Route exact path="/" component={ ProductList } />
+        <Route path="/shoppingcart" component={ ShoppingCart } />
+      </BrowserRouter>
+    </section>
   );
 }
 
--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,14 +1,16 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import cartIcon from './ShoppinCart/cartIcon.png';
 
 class ProductList extends Component {
   render() {
-    // const { } = this.props;
     return (
       <div>
         <label htmlFor="searchText" data-testid="text-input-label">
           Inclui o texto
           <input
+            placeholder="Pesquisar produto..."
             data-testid="text-input"
             type="text"
             name="searchProduct"
@@ -21,9 +23,58 @@ class ProductList extends Component {
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         </div>
+        <Link
+          to="/ShoppingCart"
+        >
+          <img
+            alt="shopping-cart"
+            data-testid="shopping-cart-button"
+            className="shopping-cart-img"
+            src={ cartIcon }
+          />
+        </Link>
       </div>
     );
   }
 }
 
 export default ProductList;
+
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// // import PropTypes from 'prop-types';
+
+// import cartIcon from '../Images/cartIcon.png';
+
+// class CartButton extends Component {
+//   render() {
+//     // const { cart } = this.props;
+//     return (
+//       <section className="cart-button-container">
+//         <Link
+//           to="/shopping-cart"
+//           data-testid="shopping-cart-button"
+//         >
+//           <img
+//             alt="shopping-cart"
+//             className="shopping-cart-img"
+//             src={ cartIcon }
+//           />
+//         </Link>
+//         <div
+//           data-testid="shopping-cart-size"
+//           className="cart-count"
+//         >
+//           {/* {cart} */}
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+// // CartButton.propTypes = {
+// //   cart: PropTypes.number.isRequired,
+// // };
+
+// export default CartButton;
+

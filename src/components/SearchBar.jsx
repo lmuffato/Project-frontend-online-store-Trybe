f4b18741f070a87fc151061d68ee8da541ed2adb
luanlsr <luanfswd@gmail.com>
2021-05-17 18:49:06 -0300

    Inserido cor no icone do carrinho

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { GrCart } from 'react-icons/gr';
+import { FaShoppingCart } from 'react-icons/fa';
+
 // import ShoppingCart from './ShoppingCart';
 
 class SearchBar extends Component {
@@ -8,15 +9,23 @@ class SearchBar extends Component {
     // const link = <Link to="./ShoppingCart" />;
     return (
       <div data-testid="home-initial-message">
-        <input
-          className="home-initial-message"
-          id="home-initial-message"
-          type="search"
-          placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
-        />
-        <Link to="./ShoppingCart">
-          <GrCart data-testid="shopping-cart-button" />
-        </Link>
+        <div className="container-searchBar">
+          <input
+            className="home-initial-message"
+            id="home-initial-message"
+            type="search"
+            placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
+          />
+          <Link to="./ShoppingCart">
+            <FaShoppingCart
+              className="cart"
+              color="#3BC18C"
+              size="1.8rem"
+              data-testid="shopping-cart-button"
+            />
+          </Link>
+        </div>
+
         <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
       </div>
     );

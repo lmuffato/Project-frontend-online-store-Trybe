053bf16883ee62a5372e7d2fcfe5f062426a53d4
Priscila <prihscapin@gmail.com>
2021-05-17 14:47:00 -0300

    feat: product list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import Product from '../components/Product';
+import SideBar from '../components/SideBar';
+import ButtonCart from '../components/ButtonCart';
+import * as api from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      products: [],
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({ query: value });
+  }
+
+  handleClick = async () => {
+    const { query } = this.state;
+    this.setState({
+      products: await api.getItemsByTerm(query),
+    });
+    console.log(this.state.products);
+  }
+
   render() {
+    const { products } = this.state;
+    console.log(products);
     return (
       <main>
         <label data-testid="home-initial-message" htmlFor="search">
           Digite algum termo de pesquisa ou escolha uma categoria.
-          <input id="search" />
+          <input id="search" data-testid="query-input" onChange={ this.handleChange } />
+          <button type="button" data-testid="query-button" onClick={ this.handleClick }>
+            Buscar
+          </button>
         </label>
+        <SideBar />
+        <ButtonCart />
+        <section>
+          {products.map(({ results }) => (<Product
+            key={ product.id }
+            product={ results }
+          />))}
+        </section>
       </main>
     );
   }

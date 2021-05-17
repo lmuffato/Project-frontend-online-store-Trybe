a148ee8218658c218bd3b959bcd492ec84d3e64d
renancosta21 <renancosta21@gmail.com>
2021-05-17 19:17:09 -0300

    feat: requirement 7

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './pages/Cart';
-import * as api from './services/api';
+// import * as api from './services/api';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <div className="App">
-          { console.log(api.getCategories()) }
+          {/* { console.log(api.getCategories()) } */}
           <Route exact path="/" component={ Home } />
           <Route path="/cart" component={ Cart } />
+          <Route
+            path="/productdetails/:id"
+            render={ (props) => <ProductDetail { ...props } /> }
+          />
         </div>
       </Switch>
     </BrowserRouter>

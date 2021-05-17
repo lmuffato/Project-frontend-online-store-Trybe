2d440aae822e3ac00bf7b9dddfbf706a487d691f
luanlsr <luanfswd@gmail.com>
2021-05-17 16:01:00 -0300

    Requisito 3 finalizado | teste do react-icons

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import ShoppingCart from './ShoppingCart';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ SearchBar } />
+        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
       </Switch>
     );
   }

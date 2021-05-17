6bf7e976d6817a5ab6fb1cd389adb5454c421a56
Orlando Flores <orlando_flores@id.uff.br>
2021-05-17 17:47:52 -0300

    fix: modifica logica de renderizar o product list para a page HOME

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,57 +1,24 @@
 import React from 'react';
-import ProductList from './ProductList';
-import * as api from '../services/api';
 
 class SearchBar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      productList: [],
-      loading: false,
-      isClicked: false,
-    };
-    this.handleList = this.handleList.bind(this);
+  render() {
+    const { onClick, onChange } = this.props;
+    return (
+      <div>
+        <input
+          data-testid="query-input"
+          type="text"
+          onChange={ onChange }
+        />
+        <button data-testid="query-button" type="button" onClick={ onClick }>
+          Pesquisar
+        </button>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
   }
-
- valueInput = ({ target }) => {
-   this.setState({
-     input: target.value,
-   });
- }
-
- handleList = async () => {
-   const { input } = this.state;
-   this.setState({ loading: true }, async () => {
-     const { results } = await api.getProductsFromCategoryAndQuery('all', input);
-     this.setState({
-       productList: results,
-       loading: false,
-       isClicked: true,
-     });
-   });
- }
-
- render() {
-   const { productList, loading, isClicked } = this.state;
-   if (loading) return (<p>Carregando...</p>);
-   return (
-     <div>
-       <input
-         data-testid="query-input"
-         type="text"
-         onChange={ (e) => this.valueInput(e) }
-       />
-       <button data-testid="query-button" type="button" onClick={ this.handleList }>
-         Pesquisar
-       </button>
-       <p data-testid="home-initial-message">
-         Digite algum termo de pesquisa ou escolha uma categoria.
-       </p>
-       <ProductList products={ productList } isClicked={ isClicked } />
-     </div>
-   );
- }
 }
 
 export default SearchBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,49 @@ import React from 'react';
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
 import CartButton from '../components/CartButton';
+import * as api from '../services/api';
+import ProductList from '../components/ProductList';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      input: '',
+      products: [],
+      isClicked: false,
+    };
+  }
+
+  handleOnChange = (event) => {
+    const { value } = event.target;
+    this.setState({ input: value });
+  }
+
+  handleClick = () => {
+    this.handleProductsList();
+  }
+
+  handleProductsList = () => {
+    const { input } = this.state;
+    this.setState({ loading: true }, async () => {
+      const { results } = await api.getProductsFromCategoryAndQuery('all', input);
+      this.setState({
+        loading: false,
+        products: results,
+        isClicked: true,
+      });
+    });
+  }
+
   render() {
+    const { products, loading, isClicked } = this.state;
+    if (loading) return (<p>Carregando...</p>);
     return (
       <>
-        <SearchBar />
+        <SearchBar onClick={ this.handleClick } onChange={ this.handleOnChange } />
         <Categories />
+        <ProductList products={ products } isClicked={ isClicked } />
         <CartButton data-testid="shopping-cart-button" />
       </>
     );

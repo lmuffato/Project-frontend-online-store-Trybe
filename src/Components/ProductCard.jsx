6dece3e25c6fa8304b53326dc6b48b6cf460e135
mallmann02 <leomallmann020302@gmail.com>
2021-05-17 18:02:19 -0300

    refactor: Each Product Passes Its Title As Value For Function

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductCard extends Component {
   render() {
-    const { title, price, imagePath } = this.props;
+    const { title, price, imagePath, onClick } = this.props;
+    
     return (
       <div data-testid="product">
         <h1>{title}</h1>
         <img src={ imagePath } alt="Imagem do produto" />
         <span>{price}</span>
+        <button onClick={onClick} value={ title } data-testid="product-add-to-cart">
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }

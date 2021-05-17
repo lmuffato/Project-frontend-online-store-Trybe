43f3798bf2a974527180a94367b93e03666f5432
Maria Baeta <m.martinsdg@gmail.com>
2021-05-17 12:10:12 -0300

    Pull request 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import * as apiUrl from '../services/api';
 import Categories from './Categories';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: '',
+      status: false,
+      products: [],
+    };
+    this.eventFilter = this.eventFilter.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleFilter();
+  }
+
+  async handleFilter() {
+    const { filter, status } = this.state;
+    const { getQuery } = await apiUrl;
+    if (status === true) {
+      const result = await getQuery(filter);
+      this.setState({
+        products: result.results,
+        filter: '',
+      });
+    }
+  }
+
+  eventFilter(event) {
+    const seach = event.target.value;
+    if (seach.length !== 0) {
+      this.setState({
+        filter: seach,
+        status: true,
+      });
+    } else {
+      this.setState({
+        status: false,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
-        <input type="search" />
+        <input type="search" data-testid="query-input" onChange={ this.eventFilter } />
+        <button type="button" data-testid="query-button" onClick={ this.handleFilter }>
+          Search
+        </button>
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
--- src/services/api.js
+++ src/services/api.js
@@ -10,3 +10,17 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const result = await dataRequery.json();
   return result;
 }
+
+export async function getQuery(query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const dataQuery = await fetch(url);
+  const result = await dataQuery.json();
+  return result;
+}
+
+export async function getCategoryId(categoryId) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const dataCategory = await fetch(url);
+  const result = await dataCategory.json();
+  return result;
+}

0ce16992cc5391e63045c609cb1ff446f3f857b6
Maria Baeta <m.martinsdg@gmail.com>
2021-05-17 15:30:17 -0300

    Request 05

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,6 +12,8 @@ class SearchBar extends React.Component {
     };
     this.eventFilter = this.eventFilter.bind(this);
     this.handleFilter = this.handleFilter.bind(this);
+    this.products = this.products.bind(this);
+    this.handleFilterCategory = this.handleFilterCategory.bind(this);
   }
 
   componentDidMount() {
@@ -20,14 +22,39 @@ class SearchBar extends React.Component {
 
   async handleFilter() {
     const { filter, status } = this.state;
-    const { getQuery } = await apiUrl;
+    const { getQuery } = apiUrl;
     if (status === true) {
       const result = await getQuery(filter);
       this.setState({
         products: result.results,
-        filter: '',
       });
     }
+  }
+
+  async handleFilterCategory(event) {
+    const category = event.target.innerText;
+    console.log(category);
+    const { getCategoryId } = apiUrl;
+    const result = await getCategoryId(category);
+    console.log(result.results);
+  }
+
+  products() {
+    const { products } = this.state;
+    if (products.length === 0) {
+      return <p>Nenhum produto foi encontrado</p>;
+    }
+    return (
+      products.map((product) => {
+        return (
+          <div data-testid="product" key={ product.id }>
+            <img src={ product.thumbnail } alt={ product.id } />
+            <h6>{ product.title }</h6>
+            <p>{` R$ ${ product.price } `}</p>
+          </div>
+        );
+      })
+    );
   }
 
   eventFilter(event) {
@@ -47,14 +74,15 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div>
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
         <input type="search" data-testid="query-input" onChange={ this.eventFilter } />
         <button type="button" data-testid="query-button" onClick={ this.handleFilter }>
           Search
         </button>
-        <h2 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h2>
-        <Categories />
+        { this.products() }
+        <Categories func={ this.handleFilterCategory } />
       </div>
     );
   }

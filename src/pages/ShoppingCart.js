52f75d96d9c95ea74757579cb326876ebfc64618
nascjoao <oi@joaonasc.dev>
2021-05-17 16:35:18 -0300

    feat: (req10) display h1 for each product in cart state

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 class ShoppingCart extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cart: [],
+    };
+  }
+
   render() {
+    const { cart } = this.state;
+    const cartIsEmpty = cart.length < 1;
     return (
-      <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+      <>
+        <h1>Carrinho de Compras</h1>
+        { cartIsEmpty ? (
+          <strong data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </strong>
+        ) : (
+          <>
+            {cart.map((product) => (
+              <h1 key={ product.id }>{ product.title }</h1>
+            ))}
+            <strong>Valor total da compra</strong>
+            <button type="button">Finalizar compra</button>
+          </>
+        )}
+      </>
     );
   }
 }

05516ce65318fd5505093b101d5696a037e480db
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-17 10:37:42 -0300

    feach Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SearchBar from './components/SearchBar';
 import HomeCart from './components/HomeCart';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {

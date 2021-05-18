c2b6e57a1db65f512af1f43ac9249733e146df25
guidugaich <guidugaich.dev@gmail.com>
2021-05-17 21:41:31 -0300

    Modificando App de função para classe

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/shopping-cart" component={ ShoppingCart } />
-        <Route exact path="/products/:id" component={ ProductDetails } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/shopping-cart" component={ ShoppingCart } />
+          <Route exact path="/products/:id" component={ ProductDetails } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;

ba2bc5d1a4bfc3cb9b9a235872ddc59977ef3f80
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-18 18:03:32 -0300

    acerto arquivos

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,4 +1,3 @@
-// Falta implementar o requisito 9 para que este seja atualizado e setado para que funcione direito
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
@@ -6,59 +5,58 @@ export default class CartProduct extends Component {
   constructor(props) {
     super(props);
 
-    const { products: { value } } = this.props;
+    const { product: { price, amount } } = this.props;
 
     this.state = {
-      valueProduct: value,
-      quantity: 1,
+      valueProduct: price,
+      quantity: amount,
     };
   }
 
   addPrice = () => {
-    const { products: { value } } = this.props;
+    const { product: { price } } = this.props;
     this.setState(({ quantity }) => ({ quantity: quantity + 1 }));
-    this.setState(({ quantity }) => ({ valueProduct: quantity * value }));
+    this.setState(({ quantity }) => ({ valueProduct: quantity * price }));
   }
 
   decreasePrice = () => {
     const { quantity } = this.state;
-    const { products: { value } } = this.props;
+    const { product: { price } } = this.props;
     if (quantity > 1) { this.setState(({ quantity }) => ({ quantity: quantity - 1 })); }
-    this.setState(({ quantity }) => ({ valueProduct: quantity * value }));
+    this.setState(({ quantity }) => ({ valueProduct: quantity * price }));
   }
 
   render() {
-    const { products: { name, imgPath, id } } = this.props;
-    const { deleteItem } = this.props;
+    const { product: { title, thumbnail }, deleteItem, id } = this.props;
     const { valueProduct } = this.state;
+    console.log(valueProduct);
+    console.log(this.props);
     return (
       <div>
         <button type="button" onClick={ deleteItem } id={ id }>
           X
         </button>
         <h2>
-          { name }
+          { title }
         </h2>
-        <image src={ imgPath } alt="Imagem Produto" />
+        <image src={ thumbnail } alt="Imagem Produto" />
         <h3>
-          { `R$ ${valueProduct}` }
+          { valueProduct }
         </h3>
-        <div>
-          <button
-            type="button"
-            data-testid="product-increase-quantity"
-            onClick={ this.addPrice }
-          >
-            +
-          </button>
-          <button
-            type="button"
-            data-testid="product-decreate-quantity"
-            onClick={ this.decreasePrice }
-          >
-            -
-          </button>
-        </div>
+        <button
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={ this.addPrice }
+        >
+          +
+        </button>
+        <button
+          type="button"
+          data-testid="product-decreate-quantity"
+          onClick={ this.decreasePrice }
+        >
+          -
+        </button>
       </div>
     );
   }
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CartProduct from '../components/CartProduct';
 
 export default class ShoppingCart extends Component {
-  constructor(props) {
-    super(props);
-
-    const productsObject = {
-      1: { name: 'Ventilador', value: 39.99, imgPath: 'https://i.mlcdn.com.br/1500x1500/020458900.jpg', id: 1 },
-      2: { name: 'Fantasma de Pelúcia', value: 20.00, imgPath: 'https://a-static.mlcdn.com.br/618x463/almofada-emoji-40cm-fantasma-pelucia-socriativos/socriativos/1055741149/303cc3507c9f6e9b7af5181f9f93ef28.jpg', id: 2 },
-      3: { name: 'Desentupidor', value: 9.99, imgPath: 'https://casatoni.vteximg.com.br/arquivos/ids/157584-1000-1000/Desentupidor-Sanitario-Grande-Bt479.jpg?v=636443492590700000', id: 3 },
-    };
-
-    this.state = {
-      cartProducts: /* false */ true,
-      products: productsObject,
-    };
-  }
 
   deleteItem = (event) => {
     const { id } = event.target;
     const { products } = this.state;
-    const items = Array.from(products).find((item) => Object.keys(products) === id ));
-    console.log('sou eu', id, items);
+    // const items = Array.from(products).find((item) => Object.keys(products) === id);
+    console.log('sou eu', id);
     // this.setState({ products: items });
   }
 
-  render() {
-    const { cartProducts, products } = this.state;
+  validateCart = () => {
+    const {
+      location: { cartItems = {} },
+    } = this.props;
+
+    if (Object.keys(cartItems).length) {
+      return Object.entries(cartItems).map((product, i) => <CartProduct key={ i } product={ product[1] } deleteItem={ this.deleteItem } id={ product.id } />);
+    }
 
     return (
-      <div>
-        { cartProducts
-          ? products.map(
-            (product, index) => <CartProduct key={ index } products={ product } deleteItem={ this.deleteItem } id={ product.id } />
-          )
-          : <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3> }
-        <div>Valor Total da Compra: </div>
-      </div>
+      <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
     );
+  };
+
+  render() {
+    return <section>{this.validateCart()}</section>;
   }
 }
+
+ShoppingCart.propTypes = {
+  location: PropTypes.shape({
+    cartItems: PropTypes.shape({
+      title: PropTypes.string,
+      price: PropTypes.number,
+      amount: PropTypes.number,
+    }),
+  }).isRequired,
+};

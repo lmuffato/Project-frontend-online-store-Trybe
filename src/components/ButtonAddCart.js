f2ae2be51f48e6749def4e5c56827d476857a771
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-18 19:28:05 -0300

    test

---
+++ src/components/ButtonAddCart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ButtonAddCart extends Component {
+  render() {
+    return (
+      <button type="button">
+        Adicionar ao Carrinho
+      </button>
+    );
+  }
+}

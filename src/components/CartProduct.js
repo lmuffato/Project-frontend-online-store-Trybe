a148e49870c1f72ac45ca28a50d3f72f2c607789
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-18 17:22:12 -0300

    feat: creating delete button - requisito-10

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,6 +1,6 @@
 // Falta implementar o requisito 9 para que este seja atualizado e setado para que funcione direito
-
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class CartProduct extends Component {
   constructor(props) {
@@ -15,49 +15,55 @@ export default class CartProduct extends Component {
   }
 
   addPrice = () => {
-    const { valueProduct } = this.state;
     const { products: { value } } = this.props;
     this.setState(({ quantity }) => ({ quantity: quantity + 1 }));
     this.setState(({ quantity }) => ({ valueProduct: quantity * value }));
   }
 
   decreasePrice = () => {
-    const { valueProduct, quantity } = this.state;
+    const { quantity } = this.state;
     const { products: { value } } = this.props;
-    if (quantity > 0) { this.setState(({ quantity }) => ({ quantity: quantity - 1 })); }
+    if (quantity > 1) { this.setState(({ quantity }) => ({ quantity: quantity - 1 })); }
     this.setState(({ quantity }) => ({ valueProduct: quantity * value }));
   }
 
   render() {
-    const { products: { name, value, imgPath } } = this.props;
+    const { products: { name, imgPath, id } } = this.props;
+    const { deleteItem } = this.props;
     const { valueProduct } = this.state;
-    console.log(valueProduct);
-    console.log(this.props);
     return (
       <div>
+        <button type="button" onClick={ deleteItem } id={ id }>
+          X
+        </button>
         <h2>
           { name }
         </h2>
         <image src={ imgPath } alt="Imagem Produto" />
         <h3>
-          { valueProduct }
+          { `R$ ${valueProduct}` }
         </h3>
-        <button
-          type="button"
-          data-testid="product-increase-quantity"
-          onClick={ this.addPrice }
-        >
-          +
-        </button>
-        <button
-          type="button"
-          data-testid="product-decreate-quantity"
-          onClick={ this.decreasePrice }
-        >
-          -
-        </button>
-        <p> { this.state.value } </p>
+        <div>
+          <button
+            type="button"
+            data-testid="product-increase-quantity"
+            onClick={ this.addPrice }
+          >
+            +
+          </button>
+          <button
+            type="button"
+            data-testid="product-decreate-quantity"
+            onClick={ this.decreasePrice }
+          >
+            -
+          </button>
+        </div>
       </div>
     );
   }
 }
+
+CartProduct.propTypes = {
+  products: PropTypes.object,
+}.isRequired;
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import CartProduct from '../components/CartProduct';
 
 export default class ShoppingCart extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    const productsObject = {
+      1: { name: 'Ventilador', value: 39.99, imgPath: 'https://i.mlcdn.com.br/1500x1500/020458900.jpg', id: 1 },
+      2: { name: 'Fantasma de Pelúcia', value: 20.00, imgPath: 'https://a-static.mlcdn.com.br/618x463/almofada-emoji-40cm-fantasma-pelucia-socriativos/socriativos/1055741149/303cc3507c9f6e9b7af5181f9f93ef28.jpg', id: 2 },
+      3: { name: 'Desentupidor', value: 9.99, imgPath: 'https://casatoni.vteximg.com.br/arquivos/ids/157584-1000-1000/Desentupidor-Sanitario-Grande-Bt479.jpg?v=636443492590700000', id: 3 },
+    };
 
     this.state = {
       cartProducts: /* false */ true,
+      products: productsObject,
     };
   }
 
+  deleteItem = (event) => {
+    const { id } = event.target;
+    const { products } = this.state;
+    const items = Array.from(products).find((item) => Object.keys(products) === id ));
+    console.log('sou eu', id, items);
+    // this.setState({ products: items });
+  }
+
   render() {
-    const { cartProducts } = this.state;
-    const productsObject = [
-      { name: 'Ventilador', value: 39.99, imgPath: 'https://i.mlcdn.com.br/1500x1500/020458900.jpg' },
-      { name: 'Fantasma de Pelúcia', value: 20.00, imgPath: 'https://a-static.mlcdn.com.br/618x463/almofada-emoji-40cm-fantasma-pelucia-socriativos/socriativos/1055741149/303cc3507c9f6e9b7af5181f9f93ef28.jpg' },
-      { name: 'Desentupidor', value: 9.99, imgPath: 'https://casatoni.vteximg.com.br/arquivos/ids/157584-1000-1000/Desentupidor-Sanitario-Grande-Bt479.jpg?v=636443492590700000' },
-    ];
+    const { cartProducts, products } = this.state;
 
     return (
       <div>
         { cartProducts
-          ? productsObject.map((product, index) => (<CartProduct key={ index } products={ product } />))
+          ? products.map(
+            (product, index) => <CartProduct key={ index } products={ product } deleteItem={ this.deleteItem } id={ product.id } />
+          )
           : <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3> }
         <div>Valor Total da Compra: </div>
       </div>

a7e27783085a39dd4abcb831d052b5dee5c50e4b
guidugaich <guidugaich.dev@gmail.com>
2021-05-17 21:47:44 -0300

    Criando componente AddToCartButton

---
+++ src/components/AddToCartButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AddToCartButton extends React.Component {
+  render() {
+    return (
+      <button type="button">
+        Adicionar ao carrinho
+      </button>
+    );
+  }
+}

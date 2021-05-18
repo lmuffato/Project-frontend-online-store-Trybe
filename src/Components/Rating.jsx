42d64a99aa3511d596d533551a1fbc9c22b53f68
Lucas André <lucas.cancado89@gmail.com>
2021-05-18 16:58:19 -0300

    Add rating

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -5,25 +5,77 @@ class Rating extends Component {
   constructor() {
     super();
     this.state = {
-      ratings: {},
+      ratings: {
+        emai: '',
+        textAre: '',
+      },
+      email: '',
+      textArea: '',
     };
   }
 
+  getLocalStorage = () => {
+    const email = localStorage.getItem('email');
+    const textArea = localStorage.getItem('textArea');
+    this.setState({
+      ratings: {
+        emai: email,
+        textAre: textArea,
+      } });
+  }
+
+  handleChangeEmail = (event) => {
+    const mail = event.target.value;
+    this.setState({ email: mail });
+  }
+
+  handleChangeTextArea = (event) => {
+    const aval = event.target.value;
+    this.setState({ textArea: aval });
+  }
+
+  handleClick = () => {
+    const { email, textArea } = this.state;
+    localStorage.setItem('email', email);
+    localStorage.setItem('textArea', textArea);
+    this.getLocalStorage();
+  }
+
   render() {
+    const { email, textArea, ratings: { emai, textAre } } = this.state;
     return (
       <div>
         <Stars />
         <form>
           <label htmlFor="email">
             Email
-            <input type="text" id="email" name="email" required />
+            <input
+              onChange={ this.handleChangeEmail }
+              value={ email }
+              type="text"
+              id="email"
+              name="email"
+              required
+            />
           </label>
           <label htmlFor="mensagem">
             Mensagem
-            <textarea type="checkbox" id="mensagem" name="mensagem" />
+            <textarea
+              data-testid="product-detail-evaluation"
+              value={ textArea }
+              onChange={ this.handleChangeTextArea }
+              type="text"
+              id="mensagem"
+              name="mensagem"
+            />
           </label>
         </form>
-        <input type="submit" value="Avaliar" />
+        <input type="submit" value="Avaliar" onClick={ this.handleClick } />
+        <div className="rating">
+          <Stars />
+          <h3>{emai}</h3>
+          <p>{textAre}</p>
+        </div>
       </div>
     );
   }

87ada13807437dda4bfb9bfa702a5477de1e6548
Willian Mendes <willianmendesbr@gmail.com>
2021-05-18 16:16:38 -0300

    feat: product comments integration

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ButtonCart from '../components/ButtonCart';
+import ProductEvaluationForm from '../components/comment/ProductEvaluationForm';
 
 export default class ProductDetails extends Component {
   render() {
@@ -17,6 +18,7 @@ export default class ProductDetails extends Component {
           <h2 data-testid="product-detail-name">{ product.title }</h2>
           <img src={ product.thumbnail } alt="Imagem do produto" />
         </div>
+        <ProductEvaluationForm />
       </section>
     );
   }

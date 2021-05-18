5345ccf2869dd1e2b7af10bd3d4c278c6bc95339
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-05-18 20:51:28 -0300

    feat: Requirement08

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class CardItem extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { item: { title, price, id } } = this.props;
+
+    const productsCart = JSON.parse(localStorage.getItem('productsCart'));
+
+    const objProduct = { title, price, id, quantity: 1 };
+
+    if (Array.isArray(productsCart)) {
+      if (productsCart.some((product) => product.id === id)) {
+        const productFound = productsCart.find((product) => product.id === id);
+        console.log(productFound);
+        productFound.quantity += 1;
+      } else {
+        productsCart.push(objProduct);
+      }
+      localStorage.setItem('productsCart', JSON.stringify(productsCart));
+    } else {
+      localStorage.setItem('productsCart', JSON.stringify([objProduct]));
+    }
+  }
+
   render() {
     const { item: { title, price, thumbnail, id } } = this.props;
 
     return (
-      <div data-testid="product" key={ id }>
-        <h2>{ title }</h2>
-        <span>{ price }</span>
-        <img src={ thumbnail } alt="alt" />
+      <div>
+        <Link data-testid="product-detail-link" to={ `/product/${id}` }>
+          <div data-testid="product" key={ id }>
+            <h2>{ title }</h2>
+            <span>{ price }</span>
+            <img src={ thumbnail } alt="alt" />
+            {/*  <Link
+              to={ {
+                pathname: '/shopping-cart',
+                search: '?sort=name',
+                state: { title, price } } }
+            >
+              Adicionar ao Carrinho
+            </Link> */}
+          </div>
+        </Link>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ this.handleClick }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,10 +3,32 @@ import { Link } from 'react-router-dom';
 import EmptyCart from '../components/EmptyCart';
 
 export default class ShoppingCart extends Component {
+  constructor() {
+    super();
+
+    this.getStorage = this.getStorage.bind(this);
+  }
+
+  getStorage() {
+    return JSON.parse(localStorage.getItem('productsCart'));
+  }
+
   render() {
+    console.log(this.getStorage());
     return (
       <div>
         <EmptyCart />
+        {this.getStorage().map((product) => (
+          <div key={ product.id }>
+            <h1 data-testid="shopping-cart-product-name">
+              {product.title}
+            </h1>
+            <h2>
+              {product.price}
+            </h2>
+            <p data-testid="shopping-cart-product-quantity">{product.quantity}</p>
+          </div>
+        ))}
         <Link to="/">Voltar</Link>
       </div>
     );

6941301441aacac9b19baefc4c5588b2949214e0
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-19 15:54:19 -0300

    fix: lint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { getProductsFromCategoryAndQuery, getCategories } from '../services/api';
 import CardItems from '../components/CardItems';
 import CategoriesList from '../components/CategoriesList';

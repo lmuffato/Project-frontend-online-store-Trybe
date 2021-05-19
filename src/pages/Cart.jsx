7c385f537603a6f0db1bc71020fd1d53b14018ba
Orlando Flores <orlando_flores@id.uff.br>
2021-05-19 16:27:23 -0300

    feat: cria logica para renderizar preço total na pagina do carrinho de compras

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import EmptyCart from '../components/EmptyCart';
 import CartItem from '../components/CartItem';
+import * as storageFunction from '../services/localStorage';
 
 class Cart extends React.Component {
   constructor() {
@@ -8,6 +9,7 @@ class Cart extends React.Component {
     this.state = {
       products: [],
       storage: false,
+      totalPrice: 0,
     };
   }
 
@@ -16,23 +18,30 @@ class Cart extends React.Component {
   }
 
   getFromLocalStorage = () => {
-    const products = JSON.parse(localStorage.getItem('products'));
+    const products = storageFunction.getProductsFromStorage();
     if (products !== null) {
       this.setState({ storage: true });
     }
     this.setState({
       products,
     });
+    this.sumPrices(products);
+  }
+
+  sumPrices = (products) => {
+    const totalPrice = products.reduce((acc, cur) => acc + cur.price, 0);
+    this.setState({ totalPrice });
   }
 
   render() {
-    const { storage, products } = this.state;
+    const { storage, products, totalPrice } = this.state;
     return (
       <div>
         {!storage && <EmptyCart />}
         { storage && products.map((product) => (
           <CartItem key={ product.id } product={ product } />
         )) }
+        <p>{`Preço total: ${totalPrice}`}</p>
       </div>
     );
   }

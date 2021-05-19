a5ad21e06e70ce79743b73b1410f0086b8469635
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-19 15:10:43 -0300

    feat: requirement 11 v3

--- src/components/product_details.js
+++ src/components/product_details.js
@@ -7,13 +7,27 @@ class ProductDetails extends React.Component {
     super();
     this.state = {
       item: '',
-      // value: '',
+      textArea: '',
+      select: 1,
     };
     this.fetchProduct = this.fetchProduct.bind(this);
   }
 
   componentDidMount() {
     this.fetchProduct();
+  }
+
+  handleChange(field, valueUpdate) {
+    this.setState({ [field]: valueUpdate });
+  }
+
+  handleSubmit() {
+    const { textArea, select } = this.state;
+    const productEvaluation = `Texto:${textArea}, Nota:${select}`;
+    const previousEvaluations = JSON.parse(localStorage.getItem('productSubmit'));
+    const evaluationUpdate = !previousEvaluations ? [] : previousEvaluations;
+    localStorage
+      .setItem('productSubmit', JSON.stringify([...evaluationUpdate, productEvaluation]));
   }
 
   async fetchProduct() {
@@ -25,34 +39,29 @@ class ProductDetails extends React.Component {
     });
   }
 
-  /*   handleChange() {
-
-  } */
-
   render() {
     const { item } = this.state;
     console.log(item);
     return (
       <div data-testid="product-detail-name">
         {item.title}
-        <form id="product-evaluation">
-          <label htmlFor="evaluation">
-            Avalie o produto
-            <select name="stars" id="evaluation" form="product-evaluation">
-              <option value={ 1 }>1</option>
-              <option value={ 2 }>2</option>
-              <option value={ 3 }>3</option>
-              <option value={ 4 }>4</option>
-              <option value={ 5 }>5</option>
-            </select>
-            <label htmlFor="product-detail-evaluation">
-              <textarea
-                data-testid="product-detail-evaluation"
-                // value={ this.state.value }
-                onChange={ this.handleChange }
-              />
-            </label>
+        <form>
+          <select
+            onChange={ (event) => this.handleChange('select', event.target.value) }
+          >
+            <option value={ 1 }>1</option>
+            <option value={ 2 }>2</option>
+            <option value={ 3 }>3</option>
+            <option value={ 4 }>4</option>
+            <option value={ 5 }>5</option>
+          </select>
+          <label htmlFor="product-detail-evaluation">
+            <textarea
+              data-testid="product-detail-evaluation"
+              onChange={ (event) => this.handleChange('textArea', event.target.value) }
+            />
           </label>
+          <input onClick={ () => this.handleSubmit() } type="submit" value="Enviar" />
         </form>
       </div>
     );

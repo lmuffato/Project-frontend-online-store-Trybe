01c78b40bf110e6546277e90ed795deaff58f7b9
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-05-19 18:10:23 -0300

    Resolvendo conflitos de merge

--- src/components/DetailedProductCard/DetailedProductCard.jsx
+++ src/components/DetailedProductCard/DetailedProductCard.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class DetailedProductCard extends Component {
   render() {
     // Use o log abaixo para verificar todas as informações que podemos usar sobre o produto
     // console.log(Object.entries(this.props));
-    const { title, price, thumbnail } = this.props;
+    const { product, callBack } = this.props;
     return (
-      <section>
-        <h2 data-testid="product-detail-name">
-          {`${title} - R$ ${price}`}
-        </h2>
-        <img src={ thumbnail } alt={ `Produto ${title}` } />
-      </section>
+      <>
+        <Link
+          to="/cartContent"
+          data-testid="shopping-cart-button"
+        >
+          <img
+            style={ { width: '40px' } }
+            src="/images/shoppingCart.png"
+            alt="Carrinho de compras"
+          />
+        </Link>
+        <section>
+          <h2 data-testid="product-detail-name">
+            {`${product.title} - R$ ${product.price}`}
+          </h2>
+          <img src={ product.thumbnail } alt={ `Produto ${product.title}` } />
+          <button
+            data-testid="product-detail-add-to-cart"
+            type="button"
+            onClick={ () => callBack(product) }
+          >
+            Adicionar ao carrinho
+          </button>
+        </section>
+      </>
     );
   }
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,46 +1,15 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import DetailedProductCard from '../components/DetailedProductCard/DetailedProductCard';
 import EvaluationForms from '../components/EvaluationForms/EvaluationForms';
 import Evaluations from '../components/Evaluations/Evaluations';
 
 class ProductDetails extends Component {
-  productCard(title, price, thumbnail) {
-    return (
-      <>
-        <Link
-          to="/cartContent"
-          data-testid="shopping-cart-button"
-        >
-          <img
-            style={ { width: '40px' } }
-            src="/images/shoppingCart.png"
-            alt="Carrinho de compras"
-          />
-        </Link>
-        <section>
-          <h2 data-testid="product-detail-name">
-            {`${title} - R$ ${price}`}
-          </h2>
-          <img src={ thumbnail } alt={ `Produto ${title}` } />
-          <button
-            data-testid="product-detail-add-to-cart"
-            type="button"
-            onClick={ () => callBack(product) }
-          >
-            Adicionar ao carrinho
-          </button>
-        </section>
-      </>
-    );
-  }
-
   render() {
-    const { location: { state: product } } = this.props;
+    const { location: { state: product }, callBack } = this.props;
     return (
       <>
-        <DetailedProductCard { ...product } />
+        <DetailedProductCard callBack={ callBack } product={ product } />
         <EvaluationForms id={ product.id } />
         <Evaluations id={ product.id } />
       </>
@@ -53,7 +22,8 @@ ProductDetails.propTypes = {
     state: PropTypes.shape({
       // A componente DetailedProductCard é responsável por validar as props que ela precisa
     }),
-  }).isRequired,
-};
+  }),
+  callBack: PropTypes.func,
+}.isRequired;
 
 export default ProductDetails;

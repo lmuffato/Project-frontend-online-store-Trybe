ba1ef6c2def2e6f8ff4f169d51bec7c475d76c33
Orlando Flores <orlando_flores@id.uff.br>
2021-05-19 16:48:57 -0300

    feat: cria logica para aumentar e diminuir a quantidade dos itens do carrinho

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,14 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quantity: 1,
+    };
+  }
+
+  handleOnclick = (operation) => {
+    const { quantity } = this.state;
+    if (operation === 'add') {
+      this.setState((prevState) => ({
+        quantity: prevState.quantity + 1,
+      }));
+    } else if (quantity > 1) {
+      this.setState((prevState) => ({
+        quantity: prevState.quantity - 1,
+      }));
+    }
+  }
+
   render() {
     const { product } = this.props;
     const { title, price, thumbnail } = product;
+    const { quantity } = this.state;
     return (
       <div>
         <img src={ thumbnail } alt={ title } />
         <p data-testid="shopping-cart-product-name">{ title }</p>
-        <p data-testid="shopping-cart-product-quantity">1</p>
+        <button
+          onClick={ () => this.handleOnclick('sub') }
+          data-testid="product-decrease-quantity"
+          type="button"
+        >
+          -
+        </button>
+        <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
+        <button
+          onClick={ () => this.handleOnclick('add') }
+          data-testid="product-increase-quantity"
+          type="button"
+        >
+          +
+        </button>
         <p>{ price }</p>
       </div>
 

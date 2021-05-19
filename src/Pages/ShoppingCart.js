e33e8143a3714ec3b69dfeff7ef37dd6b7a396dd
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-19 15:06:09 -0300

    feat: Shopping Cart upgraded

--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { func, shape } from 'prop-types';
 
 class ShoppingCart extends React.Component {
   render() {
+    const { history: { goBack } } = this.props;
     return (
-      <h4 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h4>
+      <div className="shopping-cart-container">
+        <button type="button" onClick={ goBack }>
+          <img
+            className="back"
+            src="https://image.flaticon.com/icons/png/512/64/64516.png"
+            alt="voltar"
+          />
+        </button>
+        <h4 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h4>
+      </div>
     );
   }
 }
+
+ShoppingCart.propTypes = {
+  history: shape({
+    goBack: func,
+  }).isRequired,
+};
 
 export default ShoppingCart;

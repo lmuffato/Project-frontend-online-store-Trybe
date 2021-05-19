f97fc25626c11e168f961727a01ca9de52d80229
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-19 17:42:54 -0300

    in progress: requiment 10

---
+++ src/Components/CartProduct.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CartProduct extends Component {
+  render() {
+    return (
+      <li>
+        Produto
+      </li>
+    );
+  }
+}
+
+export default CartProduct;
--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { func, shape } from 'prop-types';
 
+import CartProduct from '../Components/CartProduct';
+
 class ShoppingCart extends React.Component {
   render() {
     const { history: { goBack } } = this.props;
+    const empty = <h4 data-testid="shopping-cart-empty-message">Carrinho vazio</h4>;
+    const onOff = true;
+    const produtcs = (
+      <>
+        <header className="shopping-cart-header">
+          <img
+            className="shopping-cart"
+            src="https://img2.gratispng.com/20180425/lcq/kisspng-computer-icons-shopping-cart-5ae061983e57a6.1325375415246544882554.jpg"
+            alt="carrinho de compras"
+          />
+          <h3>Carrinho de compras</h3>
+        </header>
+
+        <CartProduct />
+      </>
+    );
+
     return (
       <div className="shopping-cart-container">
         <button type="button" onClick={ goBack }>
@@ -13,7 +32,11 @@ class ShoppingCart extends React.Component {
             alt="voltar"
           />
         </button>
-        <h4 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h4>
+
+        {
+          !onOff ? empty : produtcs
+        }
+
       </div>
     );
   }

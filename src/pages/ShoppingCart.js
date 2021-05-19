e4b3d9bfd06ae3668673feaee5020b2e63bca31c
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-19 17:52:12 -0300

    feat: Requirement10

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -6,30 +6,95 @@ export default class ShoppingCart extends Component {
   constructor() {
     super();
 
+    this.state = {
+      productsCart: [],
+    };
+    this.handleStorage = this.handleStorage.bind(this);
     this.getStorage = this.getStorage.bind(this);
+    this.decreaseQuantity = this.decreaseQuantity.bind(this);
+    this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+
+  componentDidMount() {
+    this.getStorage();
+  }
+
+  handleStorage(productsCart) {
+    localStorage.setItem('productsCart', JSON.stringify(productsCart));
   }
 
   getStorage() {
-    return JSON.parse(localStorage.getItem('productsCart'));
+    this.setState({
+      productsCart: JSON.parse(localStorage.getItem('productsCart')),
+    });
+  }
+
+  decreaseQuantity(id) {
+    const { productsCart } = this.state;
+    const productFound = productsCart.find((product) => product.id === id);
+    if (productFound.quantity > 1) {
+      productFound.quantity -= 1;
+      this.setState({ productsCart });
+      this.handleStorage(productsCart);
+    }
+    console.log(productFound);
+  }
+
+  increaseQuantity(id) {
+    const { productsCart } = this.state;
+    const productFound = productsCart.find((product) => product.id === id);
+    productFound.quantity += 1;
+    this.setState({ productsCart });
+    this.handleStorage(productsCart);
+    console.log(productFound);
+  }
+
+  deleteProduct(id) {
+    const { productsCart } = this.state;
+    const productDelete = productsCart.find((product) => product.id === id);
+    productsCart.splice(productsCart.indexOf(productDelete), 1);
+    this.setState({ productsCart });
+    this.handleStorage(productsCart);
+    console.log(productsCart);
   }
 
   render() {
-    console.log(this.getStorage());
+    const { productsCart } = this.state;
     return (
       <div>
-        { !this.getStorage()
+        { !productsCart || productsCart.length === 0
           ? <EmptyCart />
           : (
-            this.getStorage().map((product) => (
+            productsCart.map((product) => (
               <div key={ product.id }>
                 <h1 data-testid="shopping-cart-product-name">
                   {product.title}
                 </h1>
+                <button
+                  type="button"
+                  onClick={ () => this.deleteProduct(product.id) }
+                >
+                  X
+                </button>
                 <img src={ product.thumbnail } alt="product" />
                 <h2>
                   {product.price}
                 </h2>
+                <button
+                  type="button"
+                  data-testid="product-decrease-quantity"
+                  onClick={ () => this.decreaseQuantity(product.id) }
+                >
+                  -
+                </button>
                 <p data-testid="shopping-cart-product-quantity">{product.quantity}</p>
+                <button
+                  type="button"
+                  data-testid="product-increase-quantity"
+                  onClick={ () => this.increaseQuantity(product.id) }
+                >
+                  +
+                </button>
               </div>
             ))
           )}

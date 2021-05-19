ae5fbb203450e34e3737a70656709d349dd1cc3f
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-18 22:56:11 -0300

    feat: requirement11

--- src/components/product_details.js
+++ src/components/product_details.js
@@ -7,6 +7,7 @@ class ProductDetails extends React.Component {
     super();
     this.state = {
       item: '',
+      // value: '',
     };
     this.fetchProduct = this.fetchProduct.bind(this);
   }
@@ -24,15 +25,40 @@ class ProductDetails extends React.Component {
     });
   }
 
+  /*   handleChange() {
+
+  } */
+
   render() {
     const { item } = this.state;
     console.log(item);
     return (
-      <div data-testid="product-detail-name">{item.title}</div>
+      <div data-testid="product-detail-name">
+        {item.title}
+        <form id="product-evaluation">
+          <label htmlFor="evaluation">
+            Avalie o produto
+            <select name="stars" id="evaluation" form="product-evaluation">
+              <option value={ 1 }>1</option>
+              <option value={ 2 }>2</option>
+              <option value={ 3 }>3</option>
+              <option value={ 4 }>4</option>
+              <option value={ 5 }>5</option>
+            </select>
+            <label htmlFor="product-detail-evaluation">
+              <textarea
+                data-testid="product-detail-evaluation"
+                value={ this.state.value }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </label>
+        </form>
+      </div>
     );
   }
 }
-export default ProductDetails;
+
 ProductDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
@@ -41,3 +67,5 @@ ProductDetails.propTypes = {
     }).isRequired,
   }).isRequired,
 };
+
+export default ProductDetails;
